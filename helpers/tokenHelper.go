88134fef4c659c3033b4d8c5d27eaca5e81a9048
Mert Sancar <msmertsancar@gmail.com>
2024-05-10 19:22:10 +0300

    Add JWT token generation functionality
    Implemented JWT token generation functionality:
    - Added a helper function to generate JWT access and refresh tokens.
    - Utilized environment variables for secret key configuration.

---
+++ helpers/tokenHelper.go
@@ -0,0 +1,39 @@
+package helper
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/sancarmert/go-jwt-project/database"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	User_type  string
+	jwt.StandardClaims
+}
+
+var userCollection *mongoCollection = database.OpenCollection(database.Client, "user")
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		StandartClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims,
+	}
+}

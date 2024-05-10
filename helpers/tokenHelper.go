c48b94a34342b922db21f98851c7213a636c9790
Mert Sancar <msmertsancar@gmail.com>
2024-05-10 19:28:56 +0300

    Fix JWT token generation bug
    Corrected a bug in JWT token generation.

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -34,6 +34,17 @@ func GenerateAllTokens(email string, firstName string, lastName string, userType
 	}
 
 	refreshClaims := &SignedDetails{
-		StandardClaims: jwt.StandardClaims,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168).Unix(),
+		},
 	}
+	token ,err :=	jwt.NewithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+
+	if err != nil{
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err 
 }

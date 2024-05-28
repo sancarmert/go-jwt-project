9304955cb6d33f9733e3b9d278cba2a564c0761f
Mert Sancar <msmertsancar@gmail.com>
2024-05-28 16:07:31 +0300

    Authenticate()

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/sancarmert/go-jwt-project/controllers"
+	"github.com/sancarmert/go-jwt-project/middleware"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	incomingRoutes.Use(middleware.Authenticate)
-	incomingRoutes.GET("/users", controller.GetUsers())
-	incomingRoutes.GET("/users/id", controller.GetUser())
-
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users", controllers.GetUsers())
+	incomingRoutes.GET("/users/id", controllers.GetUser())
 }
